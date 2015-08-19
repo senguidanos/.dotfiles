fcb911f77b25e45c2c96e591735496871ac60408
Sebastian <senguidanos@gmail.com>
2015-08-19 10:14:56 -0500

    Fix setup script.

--- setup.sh
+++ setup.sh
@@ -1,4 +1,4 @@
 for dotfile in .gitconfig .tmux.conf .vimrc; do
-  [-e ~/$dotfile ] && mv ~/$dotfile ~/$dotfile.old
+  [ -e ~/$dotfile ] && mv ~/$dotfile ~/$dotfile.old
   ln -s ~/.dotfiles/$dotfile ~/$dotfile
 done
